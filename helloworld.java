2b9cc937b432ca65e82d7116d6b942a512fe282e
Jorge Abreu <jorgeabreu87@gmail.com>
2018-03-26 11:14:07 -0300

    java file with hello world

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+/*
+* Testing a hello world in Java.
+*/
+public class HolaMundo {
+   public static void main(String[] args) {
+      // Prints "Hola mundo!!".
+      System.out.println("Hola mundo!!");
+   }
+}
